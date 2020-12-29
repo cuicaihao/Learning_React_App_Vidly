e98a79cd160da31b1f3302d3002522c32e17c534
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2020-12-29 19:20:27 +1100

    add sorting icon and keys

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -16,16 +16,26 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc")
+      return <i className="fa fa-sort-asc" aria-hidden="true"></i>;
+    return <i className="fa fa-sort-desc" aria-hidden="true"></i>;
+  };
+
   render() {
     return (
       <thead>
         <tr>
           {this.props.columns.map((column) => (
             <th
+              className="clickable"
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
